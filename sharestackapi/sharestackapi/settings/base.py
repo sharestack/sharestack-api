d55c3b769d116a41d9a8260c692984f8fc01a1d8
Xabier Larrakoetxea <slok69@gmail.com>
2014-02-27 22:19:19 +0100

    Set devel database

--- sharestackapi/sharestackapi/settings/base.py
+++ sharestackapi/sharestackapi/settings/base.py
@@ -56,9 +56,13 @@ WSGI_APPLICATION = 'sharestackapi.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "sharestack",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
--- sharestackapi/sharestackapi/settings/local.py
+++ sharestackapi/sharestackapi/settings/local.py
@@ -2,3 +2,14 @@ from .base import *
 
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "sharestack",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
