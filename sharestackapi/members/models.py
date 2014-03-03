c6e08f034d8c10a8ed60993bca169b323598999f
Xabier Larrakoetxea <slok69@gmail.com>
2014-03-03 20:40:19 +0100

    #10 improve user verbose names

--- sharestackapi/members/models.py
+++ sharestackapi/members/models.py
@@ -8,18 +8,18 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible # For Python 3.3 abd 2.7
 class User(AbstractUser):
-    url = models.URLField(_("url"),
+    url = models.URLField(_("user url"),
                         blank=True)
-    gravatar = models.EmailField(_("gravatar"),
+    gravatar = models.EmailField(_("gravatar email"),
                                 blank=True)
     activation_token = models.CharField(
-        _("activation_token"),
+        _("activation token"),
         max_length=40,
         default=str(uuid.uuid4()),
         blank=True
     )
     reset_password_token = models.CharField(
-        _("reset_password_token"),
+        _("reset password token"),
         max_length=40,
         default=str(uuid.uuid4()),
         blank=True
