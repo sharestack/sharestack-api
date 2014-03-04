5a2147e1e8877b4181decb959bb2de7d24278d69
Xabier Larrakoetxea <slok69@gmail.com>
2014-03-04 21:59:17 +0100

    #11 Added instance model

--- sharestackapi/machines/models.py
+++ sharestackapi/machines/models.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
@@ -7,7 +5,7 @@ from django.utils.encoding import python_2_unicode_compatible
 from members.models import User
 
 
-@python_2_unicode_compatible # For Python 3.3 abd 2.7
+@python_2_unicode_compatible # For Python 3.3 and 2.7
 class Stack(models.Model):
     name = models.CharField(_("stack name"),
                             max_length=30,
@@ -18,11 +16,23 @@ class Stack(models.Model):
                                   default=False)
     sharelink = models.URLField(_("sharing link"),
                                 blank=True)
-    owner =  models.ForeignKey(User,related_name='owned_stack')
+    owner =  models.ForeignKey(User, related_name='owned_stack')
     collaborators = models.ManyToManyField(
         User,
         related_name='collaboration_stack',
         blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+@python_2_unicode_compatible # For Python 3.3 and 2.7
+class Instance(models.Model):
+    name = models.CharField(_("instance name"),
+                            max_length=30,
+                            null=False)
+    stack =  models.ForeignKey(Stack)
 
     def __str__(self):
         return self.name
