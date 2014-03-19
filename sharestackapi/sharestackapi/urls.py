820d8708ce1988c5fa4b2ae9e4459c1e939bb410
Xabier Larrakoetxea <slok69@gmail.com>
2014-03-19 20:06:08 +0100

    #41 Add user model rest api url mapping

--- sharestackapi/sharestackapi/urls.py
+++ sharestackapi/sharestackapi/urls.py
@@ -1,12 +1,20 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from . import apiv1
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sharestackapi.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'/'.join(("^api", settings.API_VERSION, "")),
+        # DRF has a bug in nested namespaces, doesn't check
+        # xxx:yyy-detail namespaces, Only yyy-detail and similar
+        # https://github.com/tomchristie/django-rest-framework/pull/1143
+        # for now without namespaces, uncomment this line when fixed
+        # include(apiv1.urlpatterns, namespace='api')),
+        include(apiv1.urlpatterns)),
 )
