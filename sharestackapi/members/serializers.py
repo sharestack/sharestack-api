692255173cc37d59702564df93aa0d7bfa0bc4e2
Xabier Larrakoetxea <slok69@gmail.com>
2014-03-20 20:37:08 +0100

    #39 Added permission & contentype from django models to the rest API

--- sharestackapi/members/serializers.py
+++ sharestackapi/members/serializers.py
@@ -1,7 +1,24 @@
-from .models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
 from rest_framework import serializers
 
+from .models import User
 
+
+# Django models
+class ContentTypeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = ContentType
+        fields = ("id", "name", "app_label", "model")
+
+
+class PermissionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Permission
+        fields = ("id", "name", "content_type", "codename")
+
+
+# Our models
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
